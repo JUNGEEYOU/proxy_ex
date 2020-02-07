204cc1155f2727335c4347e7470ef8f273b29d94
lena <junge2u@naver.com>
2020-02-07 10:29:58 +0900

    수정

--- redis/myproxy.py
+++ redis/myproxy.py
@@ -16,14 +16,13 @@ class RedisCache(State):
     """
     레디스 캐시 실제 작업
     """
-    def __init__(self, url):
-        self.url = url
-        self.cache()
+    @classmethod
+    def cache(self, url):
+        tmp_dict[url] = requests.get(url).text
 
-    def cache(self):
-        tmp_dict[self.url] = requests.get(self.url).text
-
+    @classmethod
     def handle(self, url):
+        print("-------------------" + url + "-------------------" )
         print(tmp_dict[url])
 
 
@@ -37,19 +36,14 @@ class ProxyRedis(State):
 
     def handle(self):
         if(self.url not in tmp_dict):
-            redis_cache = RedisCache(self.url)
-        return redis_cache.handle(self.url)
-
-
-def main():
-    client1 = ProxyRedis("https://python.bakyeono.net/chapter-3-4.html")
-    client1.handle()
-    client2 = ProxyRedis("https://ko.wikipedia.org/wiki/%ED%94%84%EB%A1%9D%EC%8B%9C_%ED%8C%A8%ED%84%B4")
-    client2.handle()
-    client3 = ProxyRedis("https://ko.wikipedia.org/wiki/%ED%94%84%EB%A1%9D%EC%8B%9C_%ED%8C%A8%ED%84%B4")
-    client3.handle()
-    print(tmp_dict)
+            redis_cache = RedisCache.cache(self.url)
+        return RedisCache.handle(self.url)
 
 
 if __name__ == "__main__":
-    main()
+    client1 = ProxyRedis("http://www.09women.com/")
+    client1.handle()
+    client2 = ProxyRedis("http://www.09women.com/")
+    client2.handle()
+    client3 = ProxyRedis("http://www.906studio.co.kr/")
+    client3.handle()
